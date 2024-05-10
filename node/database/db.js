db8d7f6664cf05e8cb04693e864ab577042eaf09
alejandro <lombo.alejandro@gmail.com>
2024-05-09 22:05:40 -0300

    cambio pass mysql2

--- node/database/db.js
+++ node/database/db.js
@@ -1,6 +1,6 @@
 import { Sequelize } from 'sequelize'
 
-const db = new Sequelize('svaweb','root', 'Putamadre2!', {
+const db = new Sequelize('svaweb','root', 'Milo2603!', {
     host: "localhost",
     dialect: 'mysql'
 })
