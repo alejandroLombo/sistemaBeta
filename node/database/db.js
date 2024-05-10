75e206d311c14b8365579d1924316a441b526eee
alejandro <lombo.alejandro@gmail.com>
2024-05-10 11:40:19 -0300

    cabio http en uri

--- node/database/db.js
+++ node/database/db.js
@@ -1,6 +1,6 @@
 import { Sequelize } from 'sequelize'
 
-const db = new Sequelize('svaweb','root', 'milo.2603', {
+const db = new Sequelize('svaweb','root', '', {
     host: "localhost",
     port:3306,
     dialect: 'mysql'
