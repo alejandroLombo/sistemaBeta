49a6912a34676339e6e02c40d24d00cd77014ed3
alejandro <lombo.alejandro@gmail.com>
2024-05-10 16:37:13 -0300

    cambios pass db

--- node/database/db.js
+++ node/database/db.js
@@ -1,6 +1,6 @@
 import { Sequelize } from 'sequelize'
 
-const db = new Sequelize('svaweb','root', 'milo.2603', {
+const db = new Sequelize('svaweb','admin', 'admin2709', {
     host: "localhost",
     port:3306,
     dialect: 'mysql'
