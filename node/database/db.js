027104ee84646385445af12fcf185d275b7303ac
alejandro <lombo.alejandro@gmail.com>
2024-05-09 22:09:06 -0300

    vuelta atas

--- node/database/db.js
+++ node/database/db.js
@@ -1,7 +1,7 @@
 import { Sequelize } from 'sequelize'
 
 const db = new Sequelize('svaweb','root', 'Milo2603!', {
-    host: "http://93.127.212.217/",
+    host: "localhost",
     dialect: 'mysql'
 })
 
